b75207da3607e0814d7343ba91efbad232b22414
alyssa moran <alyssa.moran2425@gmail.com>
2022-10-12 18:51:03 -0700

    class 10

--- Lesson 09/02_rock_paper_scissors/starter_code/game.js
+++ Lesson 09/02_rock_paper_scissors/starter_code/game.js
@@ -17,58 +17,72 @@ function computerChoice() {
 /* Create a function that compares the user's selection to the computer's selection.  It should then output the opponent's choice to #computerSelection and display who is the winner.
 
  */
-function compare(me, opponent) {
-let winnerText; 
 
-if (me === opponent ){
-    winnerText = "Its a Tie";
+function compare(me, opponent)  {
 
-} else if(me === 'rock') {
 
-    if (opponent === 'paper'){
-        winnerText = "Paper beats Rock";
-    } else if (opponent === 'scissors'){
-        winnerText = "Rock beats Scissors"
+    let winnerText; if (me === opponent ){
+        winnerText = "Its a Tie";
+    } 
+    
+    
+    else if(me === 'rock') {
+    
+        if (opponent === 'paper'){
+            winnerText = "Paper beats Rock";
+        } else if (opponent === 'scissors'){
+            winnerText = "Rock beats Scissors";
+        }
+    
+    
     }
-
-
-} else if (me === 'paper' ){ 
-
-    if (opponent === 'rock') {
-        winnerText = "Paper Beats Rock";
-    }else if( opponent === 'scissors'){
-        winnerText = "Scissors beats Rock"
+    
+    
+    
+    else if (me === 'paper' ){ 
+    
+        if (opponent === 'rock') {
+            winnerText = "Paper Beats Rock";
+        }else if( opponent === 'scissors'){
+            winnerText = "Scissors beats Rock";
+        }
+    
+    
+    } 
+    
+    
+    
+    else if (me === 'scissors'){
+    
+        if (opponent === "paper"){
+            winnerText = "Scissors beats Paper";
+        } else if (opponent === 'rock'){
+            winnerText = "Rock beats Scissors";
+        }
+    
     }
+    
+    
+    
+     // Bind a click event onto each of the three buttons.  It should run the compare function and then put the result to the #decsision id in the html
+    
+    document.querySelector("#scissors").addEventListener('click', function(e){
+        e.preventDefault();
+        let computerPlayerChoice = computerChoice();
+        compare("scissors", computerPlayerChoice);
+    });
+    
+    document.querySelector("#paper").addEventListener('click', function(e){
+        e.preventDefault();
+        let computerPlayerChoice = computerChoice();
+        compare("paper", computerPlayerChoice);
+    });
+    
+    document.querySelector("#rock").addEventListener('click', function(e){
+        e.preventDefault();
+        let computerPlayerChoice = computerChoice();
+        compare("rock", computerPlayerChoice); 
+    });
 
-
-} else if (me === 'scissors'){
-
-    if (opponent === "paper"){
-        winnerText = "Scissors beats Paper";
-    } else if (opponent === 'rock'){
-        winnerText = "Rock beats Scissors"
-    }
-
+    
 }
-
-
-
- // Bind a click event onto each of the three buttons.  It should run the compare function and then put the result to the #decsision id in the html
-
-document.querySelector("#scissors").addEventListener('click', function(e){
-    e.preventDefault();
-    let computerPlayerChoice = computerChoice();
-    compare("scissors", computerPlayerChoice);
-});
-
-document.querySelector("#paper").addEventListener('click', function(e){
-    e.preventDefault();
-    let computerPlayerChoice = computerChoice();
-    compare("paper", computerPlayerChoice);
-});
-
-document.querySelector("#rock").addEventListener('click', function(e){
-    e.preventDefault();
-    let computerPlayerChoice = computerChoice();
-    compare("rock", computerPlayerChoice);
-});
